17b039fb90417e7a54ada60bd54bd2233d8a4a1f
putrinakitam <nakitmunsi03@gmail.com>
2024-03-26 14:09:51 +0800

    tugas 2

---
+++ H071221040/praktikum_2/app/src/main/java/com/example/praktikum_2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.praktikum_2;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+   EditText edName, edUsername;
+   ImageView imageView;
+   Uri image;
+   Button buttonSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edName = findViewById(R.id.edit_name);
+        edUsername = findViewById(R.id.edit_username);
+        imageView = findViewById(R.id.image);
+        buttonSubmit = findViewById(R.id.submit);
+
+        ActivityResultLauncher<Intent> launcherIntentGallery = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        image = data.getData();
+                        imageView.setImageURI(image);
+                    }
+                }
+        );
+
+        imageView.setOnClickListener(view -> {
+            Intent openGallery = new Intent(Intent.ACTION_PICK);
+            openGallery.setType("image/*");
+            launcherIntentGallery.launch(openGallery);
+        });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edName.getText().toString();
+                String username = edUsername.getText().toString();
+
+                if (!name.isEmpty() && !username.isEmpty()) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("name", name);
+                    intent.putExtra("username", username);
+
+                    if (image != null) {
+                        intent.putExtra("image", image.toString());
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Harap isi gambar", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Harap isi kedua kolom", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
