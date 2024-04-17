92769729d9b2aa7a0a71ecc07b1b01ee647867de
Muhammad Ardiansyah Asrifah_H071221068 <ancakiky@gmail.com>
2024-04-17 21:31:24 +0800

    Pratikum 4 - H071221068

---
+++ H071221068/Pratikum-4/Tugas-04/app/src/main/java/com/example/tugaspratiku4/DataSource.java
@@ -0,0 +1,30 @@
+package com.example.tugaspratiku4;
+
+import java.util.ArrayList;
+
+public class DataSource {
+    public static ArrayList<Postingan> postingans = generateDummypostingans();
+
+    private static ArrayList<Postingan> generateDummypostingans() {
+
+        ArrayList<Postingan> postingans = new ArrayList<>();
+
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+        postingans.add(new Postingan("Ardhian", "Information System", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                R.drawable.otaku, R.drawable.unhasss));
+
+        return  postingans;
+    }
+//    public static ArrayList<Postingan> getPostingans() {
+//        return postingans;
+//    }
+}
